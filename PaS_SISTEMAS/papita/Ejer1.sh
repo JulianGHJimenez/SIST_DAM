975104ea79b6e926d44e3d61c1b856f1fcc583a6
Julian <juliangabrielhj@gmail.com>
2026-02-26 19:24:18 +0000

    haremos un commit por script

---
+++ PaS_SISTEMAS/papita/Ejer1.sh
@@ -0,0 +1,10 @@
+#!usr/bin/bash
+
+DIA=4
+HORAS=2
+SEGUNDOS=200
+
+TIEMPO=$((120 * 200))
+segundos=$((48 * $TIEMPO))
+
+echo $segundos total de segundo en 4 dias 2 horas y 200 segundos
---
+++ PaS_SISTEMAS/papita/Ejer2.sh
@@ -0,0 +1,9 @@
+#!usr/bin/bash
+
+echo haremos una subida al repositorio
+
+git add .
+
+git commit -m "haremos un commit por script"
+
+git push 
